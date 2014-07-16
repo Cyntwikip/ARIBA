5f5632c11c6295ac621bedc02b198783ec6442e2
giodeealbayda <giodeelyn_albayda@dlsu.edu.ph>
2014-07-16 15:17:45 +0800

    Add_Tenant update

--- GUI - Iteration 1/Dorm - GUI/src/Viewer/GUI/Add_Tenant.java
+++ GUI - Iteration 1/Dorm - GUI/src/Viewer/GUI/Add_Tenant.java
@@ -460,8 +460,8 @@ public class Add_Tenant extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         this.setVisible(false);
-        MainMenu menu = new MainMenu();
-        menu.setVisible(true);
+        TenantPage tenant = new TenantPage();
+        tenant.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
