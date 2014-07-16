2264dbe9fc5a6da62d4e52530500b3605f18c62a
giodeealbayda <giodeelyn_albayda@dlsu.edu.ph>
2014-07-17 04:11:04 +0800

    :)

--- GUI - Iteration 1/Dorm - GUI/src/Viewer/GUI/ViewTenant.java
+++ GUI - Iteration 1/Dorm - GUI/src/Viewer/GUI/ViewTenant.java
@@ -567,7 +567,7 @@ public class ViewTenant extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
+/*        try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
