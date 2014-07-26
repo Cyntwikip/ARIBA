ac2fdac79c5f1679454cdce099b2e1ab9bcb22f3
Danica Christine Corpuz <dccorpuz1@yahoo.com>
2014-07-26 22:09:25 +0800

    add and get all DAO - water reading

--- Iteration 2/Iteration 2/src/MainMenu.java
+++ Iteration 2/Iteration 2/src/MainMenu.java
@@ -1,3 +1,22 @@
+
+import Models.Beans.TenantBean;
+import Models.DAOImplementation.TenantDAOImplementation;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.sql.Blob;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemHeaders;
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +33,51 @@ public class MainMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        FileInputStream fin = null;
+        //try {
+            // TODO code application logic here
+            
+            //testing blob
+            /*
+            FileItem photo;
+            File imgfile = new File("C:\\Users\\Danica\\Documents\\heart.png");
+            fin = new FileInputStream(imgfile);
+            
+            Blob image = (Blob) fin;
+            
+            TenantDAOImplementation dao = new TenantDAOImplementation();
+            TenantBean bean = new TenantBean();
+            
+            Date date = new Date(1996-01-27);
+            
+            
+            bean.setAddress("Manila");
+            bean.setBirthday((java.sql.Date) date);
+            bean.setContact("09274672839");
+            bean.setDegree("CS-NE");
+            bean.setEmail("dccorpuz1@yahoo.com");
+            bean.setExpectedyearofgrad(2016);
+            bean.setFname("Danica");
+            bean.setGender("Female");
+            bean.setLname("Corpuz");
+            bean.setSchool("DLSU");
+            bean.setStatus("Current");
+            bean.setImage(image);
+            
+            dao.addTenant(bean);
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MainMenu.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fin.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenu.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        */
+        
     }
     
 }
--- Iteration 2/Iteration 2/src/Models/DAOImplementation/WaterDAOImplementation.java
+++ Iteration 2/Iteration 2/src/Models/DAOImplementation/WaterDAOImplementation.java
@@ -7,9 +7,16 @@
 package Models.DAOImplementation;
 
 import Models.Beans.WaterReadingBean;
+import Models.Connector.Connector;
 import Models.DAOInterface.WaterReadingDAOInterface;
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,12 +26,68 @@ public class WaterDAOImplementation implements WaterReadingDAOInterface {
 
     @Override
     public boolean addWaterReadingToRoom(WaterReadingBean water, int bill_roomID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Connector c = new Connector();
+            Connection connection = c.getConnection();
+            String query = "insert into waterreading (currentcubicpermeter, pricepercubicmeter, price, dateRead) values (?, ?, ?, ?)";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setFloat(1, water.getCurrentcubicmeter());
+            ps.setFloat(2, water.getPricepercubicmeter());
+            ps.setFloat(3, water.getPrice());
+            ps.setDate(4, water.getDateRead());
+            ps.executeUpdate();
+            connection.close();
+            
+            return true;
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(WaterDAOImplementation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+        
     }
 
     @Override
     public ArrayList<WaterReadingBean> getAllWaterBills() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Connector c = new Connector();
+            Connection connection = c.getConnection();
+            String query = "select * from waterreading";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet resultSet = ps.executeQuery();
+            
+            WaterReadingBean bean = new WaterReadingBean();
+            ArrayList<WaterReadingBean> list = new ArrayList<WaterReadingBean>();
+            
+            int waterID;
+            float currentcubicpermeter, pricepercubicmeter, price;
+            Date dateRead;
+            
+            while(resultSet.next()) {
+                waterID = resultSet.getInt("water_billID");
+                currentcubicpermeter = resultSet.getFloat("currentcubicpermeter");
+                pricepercubicmeter = resultSet.getFloat("pricepercubicmeter");
+                price = resultSet.getFloat("price");
+                dateRead = resultSet.getDate("dateRead");
+                
+                bean = new WaterReadingBean();
+                
+                bean.setWater_billID(waterID);
+                bean.setCurrentcubicmeter(currentcubicpermeter);
+                bean.setPricepercubicmeter(pricepercubicmeter);
+                bean.setPrice(price);
+                bean.setDateRead(dateRead);
+                
+                list.add(bean);
+            }
+            
+            return list;
+        } catch (SQLException ex) {
+            Logger.getLogger(WaterDAOImplementation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
     }
 
     @Override
