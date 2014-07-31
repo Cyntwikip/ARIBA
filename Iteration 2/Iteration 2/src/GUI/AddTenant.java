d7980c485a856d650dfc87c1bf029693441f942c
giodeealbayda <giodeelyn_albayda@dlsu.edu.ph>
2014-08-01 01:46:42 +0800

    Fixed variable names

--- Iteration 2/Iteration 2/src/GUI/AddTenant.java
+++ Iteration 2/Iteration 2/src/GUI/AddTenant.java
@@ -21,27 +21,27 @@ public class AddTenant extends javax.swing.JFrame {
         initComponents();
         jPanel1.setLayout(null);
 
-        jRadioButton1.setText("Male");
-        jRadioButton2.setText("Female");
+        MaleField.setText("Male");
+        FemaleField.setText("Female");
 
-        buttonGroup1.add(jRadioButton1);
-        buttonGroup1.add(jRadioButton2);
+        buttonGroup1.add(MaleField);
+        buttonGroup1.add(FemaleField);
 
         int year = Calendar.getInstance().get(Calendar.YEAR);
 
         for (int i = 0; i <= year - 1900; i++) { // remove all years
-            jComboBox5.removeItem(1900 + i);
+            YearField.removeItem(1900 + i);
         }
         for (int i = 0; i <= year - 1900; i++) {
-            jComboBox5.addItem(1900 + i);
+            YearField.addItem(1900 + i);
         }
 
         //jComboBox1
         for (int i = 2000; i <= year + 10; i++) {
-            jComboBox1.removeItem(i);
+            YearOfGraduationField.removeItem(i);
         }
         for (int i = 2000; i <= year + 10; i++) {
-            jComboBox1.addItem(i);
+            YearOfGraduationField.addItem(i);
         }
 
     }
@@ -63,23 +63,23 @@ public class AddTenant extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        MaleField = new javax.swing.JRadioButton();
+        FemaleField = new javax.swing.JRadioButton();
         jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox();
-        jTextField5 = new javax.swing.JTextField();
-        jTextField6 = new javax.swing.JTextField();
-        jComboBox3 = new javax.swing.JComboBox();
-        jComboBox4 = new javax.swing.JComboBox();
-        jComboBox5 = new javax.swing.JComboBox();
+        SurnameField = new javax.swing.JTextField();
+        FirstnameField = new javax.swing.JTextField();
+        SchoolField = new javax.swing.JTextField();
+        DegreeField = new javax.swing.JTextField();
+        YearOfGraduationField = new javax.swing.JComboBox();
+        ContactNumberField = new javax.swing.JTextField();
+        EmailAddressField = new javax.swing.JTextField();
+        MonthField = new javax.swing.JComboBox();
+        DayField = new javax.swing.JComboBox();
+        YearField = new javax.swing.JComboBox();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jTextField7 = new javax.swing.JTextField();
-        jTextField8 = new javax.swing.JTextField();
+        AddressField = new javax.swing.JTextArea();
+        GuardianNameField = new javax.swing.JTextField();
+        GuardianContactField = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -106,23 +106,23 @@ public class AddTenant extends javax.swing.JFrame {
         jPanel1.add(jButton3);
         jButton3.setBounds(560, 380, 90, 40);
 
-        jRadioButton1.setText("Male");
-        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+        MaleField.setText("Male");
+        MaleField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton1ActionPerformed(evt);
+                MaleFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(jRadioButton1);
-        jRadioButton1.setBounds(370, 270, 60, 23);
+        jPanel1.add(MaleField);
+        MaleField.setBounds(370, 270, 60, 23);
 
-        jRadioButton2.setText("Female");
-        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+        FemaleField.setText("Female");
+        FemaleField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton2ActionPerformed(evt);
+                FemaleFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(jRadioButton2);
-        jRadioButton2.setBounds(460, 270, 70, 23);
+        jPanel1.add(FemaleField);
+        FemaleField.setBounds(460, 270, 70, 23);
 
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/save.png"))); // NOI18N
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -132,64 +132,64 @@ public class AddTenant extends javax.swing.JFrame {
         });
         jPanel1.add(jButton1);
         jButton1.setBounds(660, 380, 90, 40);
-        jPanel1.add(jTextField1);
-        jTextField1.setBounds(370, 100, 170, 30);
-        jPanel1.add(jTextField2);
-        jTextField2.setBounds(370, 130, 170, 30);
-        jPanel1.add(jTextField3);
-        jTextField3.setBounds(590, 100, 150, 30);
-        jPanel1.add(jTextField4);
-        jTextField4.setBounds(590, 130, 150, 30);
+        jPanel1.add(SurnameField);
+        SurnameField.setBounds(370, 100, 170, 30);
+        jPanel1.add(FirstnameField);
+        FirstnameField.setBounds(370, 130, 170, 30);
+        jPanel1.add(SchoolField);
+        SchoolField.setBounds(590, 100, 150, 30);
+        jPanel1.add(DegreeField);
+        DegreeField.setBounds(590, 130, 150, 30);
 
-        jPanel1.add(jComboBox1);
-        jComboBox1.setBounds(650, 160, 90, 20);
-        jPanel1.add(jTextField5);
-        jTextField5.setBounds(390, 300, 140, 30);
-        jPanel1.add(jTextField6);
-        jTextField6.setBounds(380, 330, 150, 30);
+        jPanel1.add(YearOfGraduationField);
+        YearOfGraduationField.setBounds(650, 160, 90, 20);
+        jPanel1.add(ContactNumberField);
+        ContactNumberField.setBounds(390, 300, 140, 30);
+        jPanel1.add(EmailAddressField);
+        EmailAddressField.setBounds(380, 330, 150, 30);
 
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" }));
-        jComboBox3.addActionListener(new java.awt.event.ActionListener() {
+        MonthField.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" }));
+        MonthField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox3ActionPerformed(evt);
+                MonthFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(jComboBox3);
-        jComboBox3.setBounds(370, 160, 60, 20);
+        jPanel1.add(MonthField);
+        MonthField.setBounds(370, 160, 60, 20);
 
-        jComboBox4.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
-        jComboBox4.addActionListener(new java.awt.event.ActionListener() {
+        DayField.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
+        DayField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox4ActionPerformed(evt);
+                DayFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(jComboBox4);
-        jComboBox4.setBounds(430, 160, 40, 20);
+        jPanel1.add(DayField);
+        DayField.setBounds(430, 160, 40, 20);
 
-        jComboBox5.addActionListener(new java.awt.event.ActionListener() {
+        YearField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox5ActionPerformed(evt);
+                YearFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(jComboBox5);
-        jComboBox5.setBounds(470, 160, 70, 20);
+        jPanel1.add(YearField);
+        YearField.setBounds(470, 160, 70, 20);
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        AddressField.setColumns(20);
+        AddressField.setRows(5);
+        jScrollPane1.setViewportView(AddressField);
 
         jPanel1.add(jScrollPane1);
         jScrollPane1.setBounds(370, 190, 160, 70);
-        jPanel1.add(jTextField7);
-        jTextField7.setBounds(610, 190, 130, 30);
+        jPanel1.add(GuardianNameField);
+        GuardianNameField.setBounds(610, 190, 130, 30);
 
-        jTextField8.addActionListener(new java.awt.event.ActionListener() {
+        GuardianContactField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField8ActionPerformed(evt);
+                GuardianContactFieldActionPerformed(evt);
             }
         });
-        jPanel1.add(jTextField8);
-        jTextField8.setBounds(630, 220, 110, 30);
+        jPanel1.add(GuardianContactField);
+        GuardianContactField.setBounds(630, 220, 110, 30);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/addnewtenant-peg-edited.png"))); // NOI18N
         jPanel1.add(jLabel1);
@@ -212,65 +212,65 @@ public class AddTenant extends javax.swing.JFrame {
         menu.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
-    private void jComboBox4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox4ActionPerformed
+    private void DayFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DayFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox4ActionPerformed
+    }//GEN-LAST:event_DayFieldActionPerformed
 
-    private void jTextField8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField8ActionPerformed
+    private void GuardianContactFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GuardianContactFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField8ActionPerformed
+    }//GEN-LAST:event_GuardianContactFieldActionPerformed
 
-    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+    private void FemaleFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_FemaleFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton2ActionPerformed
+    }//GEN-LAST:event_FemaleFieldActionPerformed
 
-    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
+    private void MaleFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MaleFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton1ActionPerformed
+    }//GEN-LAST:event_MaleFieldActionPerformed
 
-    private void jComboBox3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox3ActionPerformed
+    private void MonthFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MonthFieldActionPerformed
         // TODO add your handling code here:
 
-        if (jComboBox3.getSelectedItem().equals("Jan")
-                || jComboBox3.getSelectedItem().equals("Mar")
-                || jComboBox3.getSelectedItem().equals("May")
-                || jComboBox3.getSelectedItem().equals("Jul")
-                || jComboBox3.getSelectedItem().equals("Aug")
-                || jComboBox3.getSelectedItem().equals("Oct")
-                || jComboBox3.getSelectedItem().equals("Dec")) {
-            jComboBox4.removeItem("29");
-            jComboBox4.removeItem("30");
-            jComboBox4.removeItem("31");
-            jComboBox4.addItem("29");
-            jComboBox4.addItem("30");
-            jComboBox4.addItem("31");
+        if (MonthField.getSelectedItem().equals("Jan")
+                || MonthField.getSelectedItem().equals("Mar")
+                || MonthField.getSelectedItem().equals("May")
+                || MonthField.getSelectedItem().equals("Jul")
+                || MonthField.getSelectedItem().equals("Aug")
+                || MonthField.getSelectedItem().equals("Oct")
+                || MonthField.getSelectedItem().equals("Dec")) {
+            DayField.removeItem("29");
+            DayField.removeItem("30");
+            DayField.removeItem("31");
+            DayField.addItem("29");
+            DayField.addItem("30");
+            DayField.addItem("31");
 
-        } else if (jComboBox3.getSelectedItem().equals("Feb")) { // feb
-            jComboBox4.removeItem("29");
-            jComboBox4.removeItem("30");
-            jComboBox4.removeItem("31");
+        } else if (MonthField.getSelectedItem().equals("Feb")) { // feb
+            DayField.removeItem("29");
+            DayField.removeItem("30");
+            DayField.removeItem("31");
         } else {
-            jComboBox4.removeItem("29");
-            jComboBox4.removeItem("30");
-            jComboBox4.removeItem("31");
-            jComboBox4.addItem("29");
-            jComboBox4.addItem("30");
+            DayField.removeItem("29");
+            DayField.removeItem("30");
+            DayField.removeItem("31");
+            DayField.addItem("29");
+            DayField.addItem("30");
 
         }
-    }//GEN-LAST:event_jComboBox3ActionPerformed
+    }//GEN-LAST:event_MonthFieldActionPerformed
 
-    private void jComboBox5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox5ActionPerformed
+    private void YearFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_YearFieldActionPerformed
         // TODO add your handling code here:
-        int year = (Integer) jComboBox5.getSelectedItem();
+        int year = (Integer) YearField.getSelectedItem();
         if ((year % 4) == 0) {
-            if (jComboBox3.getSelectedItem().equals("Feb")) {
-                jComboBox4.removeItem("29");
-                jComboBox4.addItem("29");
+            if (MonthField.getSelectedItem().equals("Feb")) {
+                DayField.removeItem("29");
+                DayField.addItem("29");
             }
         } else {
-            jComboBox4.removeItem("29");
+            DayField.removeItem("29");
         }
-    }//GEN-LAST:event_jComboBox5ActionPerformed
+    }//GEN-LAST:event_YearFieldActionPerformed
 
     /**
      * @param args the command line arguments
@@ -308,6 +308,21 @@ public class AddTenant extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea AddressField;
+    private javax.swing.JTextField ContactNumberField;
+    private javax.swing.JComboBox DayField;
+    private javax.swing.JTextField DegreeField;
+    private javax.swing.JTextField EmailAddressField;
+    private javax.swing.JRadioButton FemaleField;
+    private javax.swing.JTextField FirstnameField;
+    private javax.swing.JTextField GuardianContactField;
+    private javax.swing.JTextField GuardianNameField;
+    private javax.swing.JRadioButton MaleField;
+    private javax.swing.JComboBox MonthField;
+    private javax.swing.JTextField SchoolField;
+    private javax.swing.JTextField SurnameField;
+    private javax.swing.JComboBox YearField;
+    private javax.swing.JComboBox YearOfGraduationField;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.ButtonGroup buttonGroup3;
@@ -315,24 +330,9 @@ public class AddTenant extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JComboBox jComboBox3;
-    private javax.swing.JComboBox jComboBox4;
-    private javax.swing.JComboBox jComboBox5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField jTextField7;
-    private javax.swing.JTextField jTextField8;
     // End of variables declaration//GEN-END:variables
 }
