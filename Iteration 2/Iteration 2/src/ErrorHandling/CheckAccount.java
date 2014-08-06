5d27d828ef9e4847ba06b65fe8570747529aa934
giodeealbayda <giodeelyn_albayda@dlsu.edu.ph>
2014-08-06 23:26:33 +0800

    contact numbers updated

--- Iteration 2/Iteration 2/src/ErrorHandling/CheckAccount.java
+++ Iteration 2/Iteration 2/src/ErrorHandling/CheckAccount.java
@@ -29,8 +29,9 @@ public class CheckAccount {
     }
     
     public void checkContact(String contact, String field) throws AccountException{
-        if(!contact.matches("^(09+(22|22|25|06|07|15|17|26|27|"+
-                             "10|18|19|20|21|28|12))+\\d{7}$")){
+        if(!contact.matches("^(09+(05|06|15|16|17|25|26|27|35|36|37|96|97" // globe
+                + "|07|08|09|10|12|18|19|20|21|28|29|30|38|39|89|99|48|"+ // smart
+                             "|22|23|32|33))+\\d{7}$")){
             throw new AccountException(field);
         }
     }
